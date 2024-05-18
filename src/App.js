ac6fe88f5d10affafd266389c5f1de084bbde239
Jakub Krzeminski <qdesigner1985@gmail.com>
2024-05-18 13:53:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [email, setEmail] = useState('jkrzeminski@agh.edu.pl');
+
+    function handleChange(event) {
+        setEmail(event.target.value);
+    }
+    let length1;
+    let message;
+    if (email.length < 10) {
+        message = <div>Ale masz krótki adres!</div>;
+    } else if (email.length < 23) {
+        message = <span>Twój adres e-mail jest w sam raz </span>;
+    } else {
+        message = <span>Twój adres e-mail jest stanowczo za długi </span>;
+    }
+
+
+    length1 = email.length;
+
+    return (
+        <div>
+            <h1>System do zapisów na zajęcia</h1>
+            <h2>Twój e-mail to: {email}</h2>
+            <input type="text" value={email} onChange={handleChange}/>
+            <span class="text">Twój e-mail to: <b>{email}</b></span><span>{message}, bo ma: <b>{length1}</b> znaki.</span>
+        </div>
+
+    );
+
+
 }
 
 export default App;
