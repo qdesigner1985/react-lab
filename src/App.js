f2dbc78200f6ea5e777b2d3a2f168f9fe7384df5
Jakub Krzeminski <qdesigner1985@gmail.com>
2024-05-18 15:08:58 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
+import ReactDOM from "react-dom";
+import "milligram";
+
 
 function App() {
-    const [email, setEmail] = useState('jkrzeminski@agh.edu.pl');
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-    function handleChange(event) {
-        setEmail(event.target.value);
+  const database = [
+    {
+      username: "jkrzeminski@agh.edu.pl"
+    },
+    {
+      username: "fracz@agh.edu.pl"
     }
-    let length1;
-    let message;
-    if (email.length < 10) {
-        message = <span>Ale masz krótki adres! </span>;
-    } else if (email.length < 23) {
-        message = <span>Twój adres e-mail jest w sam raz </span>;
-    } else {
-        message = <span>Twój adres e-mail jest stanowczo za długi </span>;
+  ];
+
+  const errors = {
+    uname: "Niepoprawna nazwa użytkownika",
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    var { uname } = document.forms[0];
+    const userData = database.find((user) => user.username === uname.value);
+    var nazwa = uname.value;
+    if (userData) {
+        setIsSubmitted(true);
+      }
+    else {
+      setErrorMessages({ name: "uname", message: errors.uname });
     }
+  };
+  const handleClick = (event) => {
+      event.preventDefault();
+      var { uname } = document.forms[0];
+      const userData = database.find((user) => user.username === uname.value);
 
-
-    length1 = email.length;
-
-    return (
-        <div>
-            <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to: {email}</h2>
-            <input type="text" value={email} onChange={handleChange}/>
-            <span class="text">Twój e-mail to: <b>{email}</b></span><span>{message}, bo ma: <b>{length1}</b> znaki.</span>
-        </div>
-
+      if (userData) {
+          setIsSubmitted(true);
+        }
+      else {
+        setErrorMessages({ name: "uname", message: errors.uname });
+      }
+    };
+    function handleClick1() {
+        setIsSubmitted(false);
+      }
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
     );
 
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Adres e-mail: </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
 
+  return (
+    <div className="app">
+      <div className="login-form">
+      <h2>System do zapisów na zajęcia</h2>
+        <div className="title"></div>
+        {isSubmitted ? <div> Zostałeś poprawnie zalogowany <input value="wyloguj" onClick={handleClick1} type="button" /></div> : renderForm}
+      </div>
+    </div>
+  );
 }
 
 export default App;
